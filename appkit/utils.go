9895b47523e4614f0f38aec891a704ae7a15881e
dongliu <dongliu@kuaishou.com>
2020-11-26 21:27:03 +0800

    refactor struct types

--- appkit/utils.go
+++ appkit/utils.go
@@ -13,31 +13,40 @@ import (
 var resources = internal.NewResourceRegistry()
 
 func toNSRect(rect foundation.Rect) C.NSRect {
-	return *(*C.NSRect)(unsafe.Pointer(&rect))
+	return C.NSRect{
+		C.NSPoint{C.double(rect.X), C.double(rect.Y)},
+		C.NSSize{C.double(rect.Width), C.double(rect.Height)},
+	}
 }
 func toRect(nsRect C.NSRect) foundation.Rect {
-	return *(*foundation.Rect)(unsafe.Pointer(&nsRect))
+	return foundation.MakeRect(float64(nsRect.origin.x), float64(nsRect.origin.y),
+		float64(nsRect.size.width), float64(nsRect.size.height))
+}
+
+func toNSPoint(point foundation.Point) C.NSPoint {
+	return C.NSPoint{C.double(point.X), C.double(point.Y)}
+}
+func toPoint(point C.NSPoint) foundation.Point {
+	return foundation.Point{X: float64(point.x), Y: float64(point.y)}
 }
 
 func toNSSize(size foundation.Size) C.NSSize {
-	return *(*C.NSSize)(unsafe.Pointer(&size))
+	return C.NSSize{C.double(size.Width), C.double(size.Height)}
 }
 func toSize(size C.NSSize) foundation.Size {
-	return *(*foundation.Size)(unsafe.Pointer(&size))
+	return foundation.Size{Width: float64(size.width), Height: float64(size.height)}
 }
 
 func toNSEdgeInsets(insets foundation.EdgeInsets) C.NSEdgeInsets {
-	return *(*C.NSEdgeInsets)(unsafe.Pointer(&insets))
+	return C.NSEdgeInsets{C.double(insets.Top), C.double(insets.Left), C.double(insets.Bottom), C.double(insets.Right)}
 }
 func toEdgeInsets(insets C.NSEdgeInsets) foundation.EdgeInsets {
-	return *(*foundation.EdgeInsets)(unsafe.Pointer(&insets))
-}
-
-func toNSPoint(pointer foundation.Point) C.NSPoint {
-	return *(*C.NSPoint)(unsafe.Pointer(&pointer))
-}
-func toPoint(pointer C.NSPoint) foundation.Point {
-	return *(*foundation.Point)(unsafe.Pointer(&pointer))
+	return foundation.EdgeInsets{
+		Top:    float64(insets.top),
+		Left:   float64(insets.left),
+		Bottom: float64(insets.bottom),
+		Right:  float64(insets.right),
+	}
 }
 
 func toPointer(o foundation.Object) unsafe.Pointer {
--- foundation/geometry.go
+++ foundation/geometry.go
@@ -1,7 +1,5 @@
 package foundation
 
-import "C"
-
 // #cgo CFLAGS: -x objective-c
 // #cgo LDFLAGS: -framework Cocoa
 // #include "geometry.h"
@@ -10,137 +8,42 @@ import "math"
 
 const SIZE_MAX = math.MaxFloat64
 
+// ZeroRect for convenient
+var ZeroRect Rect
+
+// ZeroSize for convenient
+var ZeroSize Size
+
 // Point
-type Point C.NSPoint
-
-func (r *Point) SetX(x float64) {
-	r.x = C.double(x)
-}
-
-func (r *Point) X() float64 {
-	return float64(r.x)
-}
-
-func (r *Point) SetY(y float64) {
-	r.y = C.double(y)
-}
-
-func (r *Point) Y() float64 {
-	return float64(r.y)
+type Point struct {
+	X float64
+	Y float64
 }
 
 // Size
-type Size C.NSSize
-
-func (r *Size) SetWidth(v float64) {
-	r.width = C.double(v)
-}
-
-func (r *Size) Width() float64 {
-	return float64(r.width)
-}
-
-func (r *Size) SetHeight(v float64) {
-	r.height = C.double(v)
-}
-
-func (r *Size) Height() float64 {
-	return float64(r.height)
+type Size struct {
+	Width  float64
+	Height float64
 }
 
 // Rect
-type Rect C.NSRect
-
-func (r *Rect) SetX(x float64) {
-	r.origin.x = C.double(x)
-}
-
-func (r *Rect) X() float64 {
-	return float64(r.origin.x)
-}
-
-func (r *Rect) SetY(y float64) {
-	r.origin.y = C.double(y)
-}
-
-func (r *Rect) Y() float64 {
-	return float64(r.origin.y)
-}
-
-func (r *Rect) SetWidth(v float64) {
-	r.size.width = C.double(v)
-}
-
-func (r *Rect) Width() float64 {
-	return float64(r.size.width)
-}
-
-func (r *Rect) SetHeight(v float64) {
-	r.size.height = C.double(v)
-}
-
-func (r *Rect) Height() float64 {
-	return float64(r.size.height)
+type Rect struct {
+	Point
+	Size
 }
 
 // MakeRect create a Rect struct
 func MakeRect(x, y, width, height float64) Rect {
 	return Rect{
-		C.NSPoint(Point{C.double(x), C.double(y)}),
-		C.NSSize(Size{C.double(width), C.double(height)}),
+		Point: Point{x, y},
+		Size:  Size{width, height},
 	}
-}
-
-// MakeSize create a Size struct
-func MakeSize(width, height float64) Size {
-	return Size{C.double(width), C.double(height)}
-}
-
-// MakePoint create a Point struct
-func MakePoint(x, y float64) Point {
-	return Point{C.double(x), C.double(y)}
 }
 
 // EdgeInsets is a description of the distance between the edges of two rectangles.
-type EdgeInsets C.NSEdgeInsets
-
-func (e *EdgeInsets) Top() float64 {
-	return float64(e.top)
-}
-
-func (e *EdgeInsets) Left() float64 {
-	return float64(e.left)
-}
-
-func (e *EdgeInsets) Bottom() float64 {
-	return float64(e.bottom)
-}
-
-func (e *EdgeInsets) Right() float64 {
-	return float64(e.right)
-}
-
-func (e *EdgeInsets) SetTop(value float64) {
-	e.top = C.double(value)
-}
-
-func (e *EdgeInsets) SetLeft(value float64) {
-	e.left = C.double(value)
-}
-
-func (e *EdgeInsets) SetBottom(value float64) {
-	e.bottom = C.double(value)
-}
-
-func (e *EdgeInsets) SetRight(value float64) {
-	e.right = C.double(value)
-}
-
-func MakeEdgeInsets(top, left, bottom, right float64) EdgeInsets {
-	return EdgeInsets{
-		top:    C.double(top),
-		left:   C.double(left),
-		bottom: C.double(bottom),
-		right:  C.double(right),
-	}
+type EdgeInsets struct {
+	Top    float64
+	Left   float64
+	Bottom float64
+	Right  float64
 }
